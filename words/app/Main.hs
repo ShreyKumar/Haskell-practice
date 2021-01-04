bcb9bd1dece79f6f53b787c3cc0963f219f7d7b2
Shreyansh Kumar <shreyansh.k@myplanet.com>
2021-01-04 09:54:39 -0500

    finished word game

--- words/app/Main.hs
+++ words/app/Main.hs
@@ -2,8 +2,27 @@ module Main where
 
 import Lib
 import Data
+import System.IO
+import System.Random
 
 main :: IO ()
-main =
-  let gwc = gridWithCoords grid
-  in outputGrid gwc
+main = do
+  gen <- newStdGen
+  let filledInGrid = fillInBlanks gen grid
+      game = makeGame filledInGrid languages
+  hSetBuffering stdout NoBuffering
+  playTurn game
+
+playTurn game = do
+    putStrLn . formatGame $ game
+    putStr "Please enter a word> "
+    word <- getLine
+    putStrLn $ "You entered " ++ word
+    let newGame = playGame game word
+    if completed newGame then
+      putStrLn "Congratulations!"
+    else
+      playTurn newGame
+
+    putStrLn . formatGame $ newGame
+    playTurn newGame
--- words/src/Lib.hs
+++ words/src/Lib.hs
@@ -11,17 +11,77 @@ module Lib
     , gridWithCoords
     , cell2char
     , Cell(Cell, Indent)
+    , Game(gameGrid, gameWords)
+    , makeGame
+    , totalWords
+    , score
+    , completed
+    , playGame
+    , formatGame
+    , makeRandomGrid
+    , fillInBlanks
     ) where
 
 import Data.List (isInfixOf, transpose)
 import Data.Maybe (catMaybes, listToMaybe)
+import Data.Char (toLower)
+import System.Random
+import qualified Data.Map as M
 
-data Game = Game (Grid Cell) [String]
+data Game = Game {
+                gameGrid :: Grid Cell,
+                gameWords :: M.Map String (Maybe [Cell])
+            }
+            deriving Show
 
 data Cell = Cell (Integer, Integer) Char
             | Indent
             deriving (Eq, Ord, Show)
 type Grid a = [[a]]
+
+makeGame :: Grid Char -> [String] -> Game
+makeGame grid words =
+    let gwc = gridWithCoords grid
+        tuplify word = (word, Nothing)
+        list = map tuplify words
+        dict = M.fromList list
+    in Game gwc dict
+
+totalWords :: Game -> Int
+totalWords game = length . M.keys $ gameWords game
+
+score :: Game -> Int
+score game = length . catMaybes . M.elems $ gameWords game
+
+completed :: Game -> Bool
+completed game = score game == totalWords game
+
+playGame :: Game -> String -> Game
+playGame game word | not $ M.member word (gameWords game) = game
+playGame game word =
+    let grid = gameGrid game
+        foundWord = findWord grid word
+    in case foundWord of
+        Nothing -> game
+        Just cs ->
+            let dict = gameWords game
+                newDict = M.insert word foundWord dict
+            in game { gameWords = newDict }
+
+formatGame :: Game -> String
+formatGame game = formatGameGrid game ++ "\n\n" ++ (show $ score game) ++ "/" ++ (show $ totalWords game)
+
+makeRandomGrid gen =
+    let (gen1, gen2) = split gen
+        row = randomRs ('A', 'Z') gen1
+    in row : makeRandomGrid gen2
+
+fillInBlanks gen grid =
+    let r = makeRandomGrid gen
+        fill '_' r = r
+        fill c _ = c
+    in zipOverGridWith fill grid r 
+
 
 zipOverGrid :: Grid a -> Grid b -> Grid (a, b)
 zipOverGrid = zipWith zip
@@ -44,6 +104,16 @@ gridWithCoords = zipOverGridWith Cell coordsGrid
 
 outputGrid :: Grid Cell -> IO()
 outputGrid grid = putStrLn (formatGrid grid)
+
+formatGameGrid game =
+    let grid = gameGrid game
+        dict = gameWords game :: M.Map String (Maybe [Cell])
+        cellSet = concat . catMaybes . M.elems $ dict
+        formatCell cell =
+            let char = cell2char cell
+            in if cell `elem` cellSet then char else toLower char
+        charGrid = mapOverGrid formatCell grid
+    in unlines charGrid
 
 formatGrid :: Grid Cell -> String
 formatGrid = unlines . mapOverGrid cell2char
